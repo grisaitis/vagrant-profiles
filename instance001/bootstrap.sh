9ec5d504b8eabf5d6d1495ea78c75bf7be39c99e
William Grisaitis <wgrisaitis@gmail.com>
2015-05-28 19:40:04 -0400

    first commit

---
+++ instance001/bootstrap.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+apt-add-repository -y ppa:webupd8team/java
+apt-get update
+apt-get install -y oracle-java8-set-default
+
+
---
+++ instance003/bootstrap.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+sudo apt-add-repository -y ppa:webupd8team/java
+sudo apt-get update -y
+sudo apt-get install -y oracle-java8-set-default
+
+
